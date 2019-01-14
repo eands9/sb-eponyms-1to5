c7fb7841167b1e00ee397940beb3f5e6757db855
Eric Hernandez <eands9@yahoo.com>
2019-01-14 09:18:55 -0600

    eponyms4

--- SpellingBee-4th/Model/WordBank.swift
+++ SpellingBee-4th/Model/WordBank.swift
@@ -12,15 +12,15 @@ class WordBank{
     var list = [Word]()
     
     init(){
-        list.append(Word(word:"panglossian", sentence:""))
-        list.append(Word(word:"quixote", sentence:""))
-        list.append(Word(word:"jeremiad", sentence:""))
-        list.append(Word(word:"hector", sentence:""))
-        list.append(Word(word:"geronimo", sentence:""))
-        list.append(Word(word:"shrapnel", sentence:""))
-        list.append(Word(word:"vulcanize", sentence:""))
-        list.append(Word(word:"frankenstein", sentence:""))
-        list.append(Word(word:"boswell", sentence:""))
-        list.append(Word(word:"ampere", sentence:""))
+        list.append(Word(word:"cupid", sentence:""))
+        list.append(Word(word:"fletcherism", sentence:""))
+        list.append(Word(word:"yahoo", sentence:""))
+        list.append(Word(word:"diesel", sentence:""))
+        list.append(Word(word:"bandersnatch", sentence:""))
+        list.append(Word(word:"crusoe", sentence:""))
+        list.append(Word(word:"mentor", sentence:""))
+        list.append(Word(word:"dracula", sentence:""))
+        list.append(Word(word:"forsythia", sentence:""))
+        list.append(Word(word:"madeleine", sentence:""))
     }
 }
