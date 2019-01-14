7d3e13222d3f155a3247c8427e23a54001396098
Eric Hernandez <eands9@yahoo.com>
2019-01-13 22:45:04 -0600

    eponyms1

--- SpellingBee-4th/Model/WordBank.swift
+++ SpellingBee-4th/Model/WordBank.swift
@@ -12,10 +12,14 @@ class WordBank{
     var list = [Word]()
     
     init(){
-        list.append(Word(word:"inane", sentence:""))
-        list.append(Word(word:"relevant", sentence:""))
-        list.append(Word(word:"impetuous", sentence:""))
-        list.append(Word(word:"ambivalent", sentence:""))
-        list.append(Word(word:"dejected", sentence:""))
+        list.append(Word(word:"praline", sentence:""))
+        list.append(Word(word:"magnolia", sentence:""))
+        list.append(Word(word:"boysenberry", sentence:""))
+        list.append(Word(word:"hosta", sentence:""))
+        list.append(Word(word:"poinsettia", sentence:""))
+        list.append(Word(word:"macadamia", sentence:""))
+        list.append(Word(word:"salmonella", sentence:""))
+        list.append(Word(word:"newton", sentence:""))
+        list.append(Word(word:"saxophone", sentence:""))
     }
 }
